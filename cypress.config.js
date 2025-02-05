8ba86c503689dbf23d808e453b0b3fe348d73512
Adedeji Oluwaseyi Miracle <adedejichristabel01@gmail.com>
2025-02-05 13:11:10 +0100

    add disep4.0 test

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+async function setupNodeEvents(on, config) {
+await preprocessor.addCucumberPreprocessorPlugin(on, config);
+on("file:preprocessor", browserify.default(config));
+return config;
+}
+
+module.exports = defineConfig({
+        env: {username: "Christabel",
+  password: "123456", url: "http://www.demoblaze.com/"},
+  projectId: "2en9j5",
+        e2e: {
+            experimentalRunAllSpecs:true,
+          
+       
+            setupNodeEvents,
+            specPattern: 'cypress/integration/BDD/*.feature',
+      },
+});
